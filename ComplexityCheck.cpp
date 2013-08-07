52ccabb0496922e7e786e6ba4aaad3074a6aadaa
Erik Lovlie <erilov01@localhost.localdomain>
2013-08-07 18:36:30 +0200

    Output YAML.

--- ComplexityCheck.cpp
+++ ComplexityCheck.cpp
@@ -1,10 +1,7 @@
-// Declares clang::SyntaxOnlyAction.
 #include "clang/Frontend/FrontendActions.h"
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
-// Declares llvm::cl::extrahelp.
 #include "llvm/Support/CommandLine.h"
-
 #include "clang/AST/ASTContext.h"
 #include "clang/ASTMatchers/ASTMatchers.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
@@ -14,17 +11,15 @@ using namespace clang;
 using namespace clang::ast_matchers;
 using namespace clang::tooling;
 
-// CommonOptionsParser declares HelpMessage with a description of the common
-// command-line options related to the compilation database and input files.
-// It's nice to have this help message in all tools.
 static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
-
-// A help message for this specific tool can be added afterwards.
-static cl::extrahelp MoreHelp("\nMore help text...");
+static cl::extrahelp MoreHelp("\nThis tool outputs some useful software metrics for C++ code in YAML.\n");
 
 DeclarationMatcher FunctionMatcher =
 	functionDecl(isDefinition()).bind("functionDef");
 
+// This class outputs the metrics to stdout in YAML. To keep things simple the YAML is output
+// using the normal diagnostic output stuff. Can use the llvm YAML library later if there's
+// a benefit to that (e.g. if we need to read a configuration YAML file).
 class FunctionPrinter : public MatchFinder::MatchCallback {
 public :
 
@@ -37,11 +32,12 @@ virtual void run(const MatchFinder::MatchResult &Result) {
 			return;
 		FullSourceLoc endLocation = Result.Context->getFullLoc(fun->getLocEnd());
 		if (startLocation.isValid() && endLocation.isValid() ){
-			llvm::outs() << "Found declaration for function "
-				<< Result.Context->getSourceManager().getFilename(startLocation) << ":"
-				<< fun->getNameInfo().getName().getAsString() << " at ("
-				<< startLocation.getSpellingLineNumber() << "-"
-				<< endLocation.getSpellingLineNumber() << ")\n";
+			llvm::outs() << "- { "
+				<< "file_name: " << Result.Context->getSourceManager().getFilename(startLocation) << ", "
+				<< "fun_name: " << fun->getNameInfo().getName().getAsString() << ", "
+				<< "line_start: " << startLocation.getSpellingLineNumber() << ", "
+				<< "line_end: " << endLocation.getSpellingLineNumber()
+				<< " }\n";
 		}
 	}
 }
