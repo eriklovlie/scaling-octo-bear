0e2c3281d4cfab96f6a4607c749d07c74bb53557
Erik Lovlie <Erik.Lovlie@arm.com>
2014-07-07 18:35:34 +0200

    Added ability to tag functions as exempt.

--- get-long-functions.py
+++ get-long-functions.py
@@ -3,19 +3,34 @@ import os.path
 import json
 import glob
 
-threshold = 100
+def get_long(json_files):
+    threshold = 100
+    longfun = {}
+    for path in json_files:
+        print "Reading {}".format(path)
+        with open(path) as f:
+            doc = json.load(f)
+        for func in doc:
+            length = func['line_end'] - func['line_start']
+            if length > threshold:
+                fun = "{}:{}".format(os.path.basename(func['file_name']), func['fun_name'])
+                if exempt(func):
+                    print "Skipping exempt function: {}".format(fun)
+                else:
+                    longfun[fun] = length
+    return longfun
 
-longfun = {}
+def exempt(func):
+    # Check if a function has been annotated as exempt from the style check.
+    with open(func['file_name']) as f:
+        lines = f.readlines()
+    line_start = func['line_start']
+    if line_start > 0:
+        prev_line = lines[line_start - 1]
+        return "[STYLE-CHECK IGNORE LONG FUNCTION]" in prev_line
+    return False
 
-for path in glob.glob("*_metrics.json"):
-    print "Reading {}".format(path)
-    with open(path) as f:
-        doc = json.load(f)
-    for func in doc:
-        length = func['line_end'] - func['line_start']
-        if length > threshold:
-            fun = "{}:{}".format(os.path.basename(func['file_name']), func['fun_name'])
-            longfun[fun] = length
+longfun = get_long(glob.glob("*_metrics.json"))
 
 with open('long_functions.json', 'w') as f:
     json.dump(longfun, f)
